19051e88c6eb8b1072c2b649da50049b74d3b86b
furqatbek13 <furqatmatkarimov18@gmail.com>
2024-10-01 22:49:15 +0500

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,60 +1,38 @@
-// function main(son1 , son2) {
-//     if (son1 > son2){
-//         let res = son1 / son2
-//         return res
+///// UYGA VAZIFA /////
+// 1-misol
+// function quvvat(a, n) {
+//     return Math.pow(a, n);
+// }
+// console.log(quvvat(3, 5));
+
+
+// 2-misol
+// function mean(a, b) {
+//     let arithmetic = (a + b) / 2; 
+//     let geometric = Math.sqrt(a * b); 
+//     return [arithmetic, geometric];
+// }
+// let result = mean(12, 48);
+// console.log(result[0]); 
+// console.log(result[1]); 
+
+
+
+// 3-misol
+// function sign(n) {
+//     if (n > 0) {
+//         return 1;
+//     } else if (n < 0) {
+//         return -1;
+//     } else {
+//         return 0;
 //     }
-//     return son2
 // }
-// let result = main(20,10);
-// console.log(result);
+
+// console.log(sign(10));  
+// console.log(sign(-2)); 
+// console.log(sign(0));   
 
 
-// function main(n1 , n2) {
-//     if (n1 > n2) { 
-//         return n1
-//     }else if(n1< n2) {
-//         return n2
-//     }
-// }
-// main(30,20);
-// console.log(main(30,20));
+// 4-misol
 
-
-// function main(n) {
-//     for (let i = 0; i <= n; i++) {
-//         console.log(i);
-        
-//     }
-// }
-// main(10);
-
-
-// function getText(name) {
-//     return "salom " + name
-// }
-// let getName = getText
-// console.log(getName("Shahzod"));
-
-
-// (function(name) {
-//     console.log("Salom  " + name);
-// })("Shahzod");
-
-
-// let name = function() {
-//     console.log("salom boshachi");
-// }
-// name();
-
-
-// let main = () => {
-//     console.log("salom boshachi");
-// }
-// main();
-
-
-
-function quvvat(a, n) {
-    return Math.pow(a, n);
-}
-console.log(quvvat(3, 5));
