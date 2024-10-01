6b28ebc15f02352a9e26a2fe5e08beb949174ab7
furqatbek13 <furqatmatkarimov18@gmail.com>
2024-10-01 22:40:14 +0500

    first commit

---
+++ script/script.js
@@ -0,0 +1,60 @@
+// function main(son1 , son2) {
+//     if (son1 > son2){
+//         let res = son1 / son2
+//         return res
+//     }
+//     return son2
+// }
+// let result = main(20,10);
+// console.log(result);
+
+
+// function main(n1 , n2) {
+//     if (n1 > n2) { 
+//         return n1
+//     }else if(n1< n2) {
+//         return n2
+//     }
+// }
+// main(30,20);
+// console.log(main(30,20));
+
+
+// function main(n) {
+//     for (let i = 0; i <= n; i++) {
+//         console.log(i);
+        
+//     }
+// }
+// main(10);
+
+
+// function getText(name) {
+//     return "salom " + name
+// }
+// let getName = getText
+// console.log(getName("Shahzod"));
+
+
+// (function(name) {
+//     console.log("Salom  " + name);
+// })("Shahzod");
+
+
+// let name = function() {
+//     console.log("salom boshachi");
+// }
+// name();
+
+
+// let main = () => {
+//     console.log("salom boshachi");
+// }
+// main();
+
+
+
+function quvvat(a, n) {
+    return Math.pow(a, n);
+}
+console.log(quvvat(3, 5));
